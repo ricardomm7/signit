34f1c1ae668ad17a17138e499ecc3aace7329007
Samara Miranda <samara23.miranda@hotmail.com>
2025-03-30 20:57:16 +0100

    #1 update generate_preview_image

--- modules/utils.py
+++ modules/utils.py
@@ -5,4 +5,4 @@ def read_csv_data(csv_path):
     df = pd.read_csv(csv_path)
     if 'text' not in df.columns:
         raise ValueError("CSV file must contain a 'text' column.")
-    return df
+    return df   
